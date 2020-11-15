daa646ee92134b106a5360517590ad34769fe176
andrenasx <andre2000.an@gmail.com>
2020-11-15 11:49:59 +0000

    append a list if not empty predicate

--- Talpa/src/utils.pl
+++ Talpa/src/utils.pl
@@ -1,33 +1,41 @@
 %Replaces given Value (content) in Index position of the List
 replaceInList([_H|T], 0, Value, [Value|T]).
 replaceInList([H|T], Index, Value, [H|TNew]) :-
-        Index > 0,
-        Index1 is Index - 1,
-        replaceInList(T, Index1, Value, TNew).
+	Index > 0,
+	Index1 is Index - 1,
+	replaceInList(T, Index1, Value, TNew).
 
 %Replaces Value (content) in given Row and Column of the Matrix
 replaceInMatrix([H|T], 0, Col, Value, [HNew|T]) :-
-        replaceInList(H, Col, Value, HNew).
+	replaceInList(H, Col, Value, HNew).
 
 replaceInMatrix([H|T], Row, Col, Value, [H|TNew]) :-
-        Row > 0,
-        Row1 is Row - 1,
-        replaceInMatrix(T, Row1, Col, Value, TNew).
+	Row > 0,
+	Row1 is Row - 1,
+	replaceInMatrix(T, Row1, Col, Value, TNew).
 
 %Gets Value (content) in Index position of the List
 getValueFromList([H|_T], 0, Value) :-
-        Value = H.
+	Value = H.
 
 getValueFromList([_H|T], Index, Value) :-
-        Index > 0,
-        Index1 is Index - 1,
-        getValueFromList(T, Index1, Value).
+	Index > 0,
+	Index1 is Index - 1,
+	getValueFromList(T, Index1, Value).
 
 %Gets Value (content) in given Row and Column of the Matrix
 getValueFromMatrix([H|_T], 0, Col, Value) :-
-        getValueFromList(H, Col, Value).
+	getValueFromList(H, Col, Value).
 
 getValueFromMatrix([_H|T], Row, Col, Value) :-
-        Row > 0,
-        Row1 is Row - 1,
-        getValueFromMatrix(T, Row1, Col, Value).
+	Row > 0,
+	Row1 is Row - 1,
+	getValueFromMatrix(T, Row1, Col, Value).
+
+isEmpty(List):-
+	length(List, Size),
+	Size=0.
+
+appendListNotEmpty(L1, [], L1).
+appendListNotEmpty(L1, L2, L12):-
+	append(L1, [L2], L12).
