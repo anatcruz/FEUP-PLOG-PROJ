2f52f4af21bf902d61e82b162a42676823b73154
anatcruz <anateresacruz2000@gmail.com>
2020-11-15 15:50:11 +0000

    Made functions isPlayer and isEnemy, look prettier

--- Talpa/src/logic.pl
+++ Talpa/src/logic.pl
@@ -1,9 +1,16 @@
+isPlayer(Board, Row, Column, Player) :-
+    getValueFromMatrix(Board, Row, Column, Value),
+    Player is Value.
+
+isEnemy(Board, Row, Column, Player) :-
+    getValueFromMatrix(Board, Row, Column, Enemy),
+    Enemy is -Player.
+    
 /*checks if the player is selecting his own piece
 if not, then he is asked again to input the position of the piece he wants to move
 */
 validateContent(Board, Size, SelRow, SelColumn, Player, FinalRow, FinalColumn) :-
-    getValueFromMatrix(Board, SelRow, SelColumn, Value),
-    Player is Value,
+    isPlayer(Board, SelRow, SelColumn, Player),
     verifyPossibleMove(Board, Size, SelRow, SelColumn, Player, ListOfMoves),
     FinalRow is SelRow, FinalColumn is SelColumn;
     (
@@ -27,32 +34,28 @@ verifyPossibleMove(GameState, Size, SelRow, SelColumn, Player, ListOfMoves) :-
  
 checkDownMove(GameState, Size, Row, Col, Player, DownMove):-
     Row>0, NewRow is Row-1,
-    getValueFromMatrix(GameState, NewRow, Col, Enemy),
-    Enemy is -Player,
+    isEnemy(GameState, NewRow, Col, Player),
     append([], [NewRow, Col], DownMove).
 
 checkDownMove(GameState, Size, Row, Col, Player, []).
 
 checkUpMove(GameState, Size, Row, Col, Player, UpMove):-
     Row<Size, NewRow is Row+1,
-    getValueFromMatrix(GameState, NewRow, Col, Enemy),
-    Enemy is -Player,
+    isEnemy(GameState, NewRow, Col, Player),
     append([], [NewRow, Col], UpMove).
 
 checkUpMove(GameState, Size, Row, Col, Player, []).
 
 checkLeftMove(GameState, Size, Row, Col, Player, LeftMove):-
     Col>0, NewCol is Col-1,
-    getValueFromMatrix(GameState, Row, NewCol, Enemy),
-    Enemy is -Player,
+    isEnemy(GameState, Row, NewCol, Player),
     append([], [Row, NewCol], LeftMove).
 
 checkLeftMove(GameState, Size, Row, Col, Player, []).
 
 checkRightMove(GameState, Size, Row, Col, Player, RightMove):-
     Col<Size, NewCol is Col+1,
-    getValueFromMatrix(GameState, Row, NewCol, Enemy),
-    Enemy is -Player,
+    isEnemy(GameState, Row, NewCol, Player),
     append([], [Row, NewCol], RightMove).
 
 checkRightMove(GameState, Size, Row, Col, Player, []).
@@ -64,12 +67,12 @@ when the movement is within the same column, the player can only select the posi
 when not given a valid position the player is asked again to write the position to move to
 */
 verifyOrtMove(SelBoard, Size, Player, SelRow, SelColumn, MovRow, MovColumn, FinalRow, FinalColumn) :-
-    getValueFromMatrix(SelBoard, MovRow, MovColumn, Enemy),
+    isEnemy(SelBoard, MovRow, MovColumn, Player),
     (
         (MovRow=:=SelRow, (MovColumn=:=SelColumn+1 ; MovColumn=:=SelColumn-1));  /*Same row*/
         (MovColumn=:=SelColumn, (MovRow=:=SelRow+1 ; MovRow=:=SelRow-1)) /*Same column */
     ),
-    Enemy is -Player, FinalRow is MovRow, FinalColumn is MovColumn;
+    FinalRow is MovRow, FinalColumn is MovColumn;
     (
         write('\nERROR! That is not a valid move!\n \nMove to:\n'),
         manageRow(NewRow, Size),
