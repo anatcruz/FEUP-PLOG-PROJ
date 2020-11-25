46520367ca067196bce20819eba989a8d4b123b4
andrenasx <andre2000.an@gmail.com>
2020-11-25 16:03:58 +0000

    checkWinner(-1) using transpose

--- Talpa/src/play.pl
+++ Talpa/src/play.pl
@@ -45,62 +45,40 @@ move(GameState, Player, Row-Column, NewGameState):-
 
     %Check victory from players
 %Check victory from the current player first (last round enemy)
-game_over(Board, Size, Player, Player):-
-    checkWinner(Player, Board, Size, 0, 0).
+game_over(GameState, Size, Player, Player):-
+    checkWinner(Player, GameState, Size, 0, 0).
 
 %Check victory from the enemy after
-game_over(Board, Size, Player, Enemy):-
+game_over(GameState, Size, Player, Enemy):-
     Enemy is -Player,
-    checkWinner(Enemy, Board, Size, 0, 0).
+    checkWinner(Enemy, GameState, Size, 0, 0).
 
 
-%Check if red player (O) won, using checkRedPath to avaliate board after floodfill
-checkWinner(1, Board, Size, Row, Col):-
+%Check if red player (O) won, using checkHorizontalPath to avaliate board after floodfill
+checkWinner(1, GameState, Size, Row, Col):-
     Row < Size,
-    tryFloodFill(Board, Size, Row, Col, FinalBoard),
+    tryFloodFill(GameState, Size, Row, Col, FinalGameState),
     FinalCol is Size-1,
-    checkRedPath(FinalBoard, Size, 0, FinalCol).
+    checkHorizontalPath(FinalGameState, 0, FinalCol).
 
-%If checkRedPath failed check next row
-checkWinner(1, Board, Size, Row, Col):-
+%If FloodFill try or checkHorizontalPath failed check next row
+checkWinner(1, GameState, Size, Row, Col):-
     Row < Size,
     NextRow is Row + 1,
-    checkWinner(1, Board, Size, NextRow, Col).
+    checkWinner(1, GameState, Size, NextRow, Col).
 
-
-%Check if blue player (X) won, using checkBluePath to avaliate board after floodfill
-checkWinner(-1, Board, Size, Row, Col):-
-    Col < Size,
-    tryFloodFill(Board, Size, Row, Col, FinalBoard),
-    FinalRow is Size-1,
-    checkBluePath(FinalBoard, Size, FinalRow, 0).
-
-%If checkBluePath failed check next column
-checkWinner(-1, Board, Size, Row, Col):-
-    Col < Size,
-    NextCol is Col + 1,
-    checkWinner(-1, Board, Size, Row, NextCol).
-
+%Check if blue player (X) won, transposing matrix and using checkWinner for red player to check for an horizontal path
+checkWinner(-1, GameState, Size, Row, Col):-
+    transpose(GameState, Transpose),
+    checkWinner(1, Transpose, Size, Row, Col).
     
 %Auxiliar to check if there is a floodfill replaced char in the final column, meaning a path for the red player
-checkRedPath(Board, Size, Row, FinalCol):-
-    Row < Size,
-    getValueFromMatrix(Board, Row, FinalCol, 2).
+checkHorizontalPath(GameState, Row, FinalCol):-
+    Row =< FinalCol,
+    getValueFromMatrix(GameState, Row, FinalCol, 2).
 
 %If failed, check for a path in the next row and final column
-checkRedPath(Board, Size, Row, FinalCol):-
-    Row < Size,
+checkHorizontalPath(GameState, Row, FinalCol):-
+    Row =< FinalCol,
     NextRow is Row + 1,
-    checkRedPath(Board, Size, NextRow, FinalCol).
-
-
-%Auxiliar to check if there is a floodfill replaced char in the final row, meaning a path for the blue player
-checkBluePath(Board, Size, FinalRow, Col):-
-    Col < Size,
-    getValueFromMatrix(Board, FinalRow, Col, 2).
-
-%If failed, check for a path in the next column and final row
-checkBluePath(Board, Size, FinalRow, Col):-
-    Col < Size,
-    NextCol is Col + 1,
-    checkBluePath(Board, Size, FinalRow, NextCol).
+    checkHorizontalPath(GameState, NextRow, FinalCol).
