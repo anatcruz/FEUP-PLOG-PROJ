2df7c080de20831f468b20b23b2a89bb7588c758
anatcruz <anateresacruz2000@gmail.com>
2020-11-25 17:13:44 +0000

    Continuing

--- Talpa/src/menu.pl
+++ Talpa/src/menu.pl
@@ -42,108 +42,10 @@ mainMenu :-
     write('|                                    |\n'),
     write('|             0. Exit                |\n'),
     write('|                                    |\n'),
+    write('|                                    |\n'),
     write('|____________________________________|\n\n'),
     selectMenuOption(3, ValidOption),
     mainMenuAction(ValidOption).
-
-%Displays Play menu
-playersMenu :-
-    write('\33\[2J'),
-    write('\n\n ____________________________________\n'),
-    write('|                                    |\n'),
-    write('|     _____       _                  |\n'),
-    write('|    |_   _|__ _ | | ____  __ _      |\n'),
-    write('|      | | / _` || ||  _ |/ _` |     |\n'),
-    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
-    write('|                   |_|              |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|        1. Player VS Player         |\n'),
-    write('|                                    |\n'),
-    write('|        2. Player VS Computer       |\n'),
-    write('|                                    |\n'),
-    write('|       3. Computer VS Computer      |\n'),
-    write('|                                    |\n'),
-    write('|            0. Main Menu            |\n'),
-    write('|                                    |\n'),
-    write('|____________________________________|\n\n'),
-    selectMenuOption(3, ValidOption),
-    menuAction(ValidOption).
-
-%Displays board size menu
-boardSizeMenu(ValidOption, GameState, Size):-
-    write('\33\[2J'),
-    write('\n\n ____________________________________\n'),
-    write('|                                    |\n'),
-    write('|     _____       _                  |\n'),
-    write('|    |_   _|__ _ | | ____  __ _      |\n'),
-    write('|      | | / _` || ||  _ |/ _` |     |\n'),
-    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
-    write('|                   |_|              |\n'),
-    write('|                                    |\n'),
-    write('|    Choose the board dimensions     |\n'),
-    write('|                                    |\n'),
-    write('|              1. 6 x 6              |\n'),
-    write('|                                    |\n'),
-    write('|              2. 8 x 8              |\n'),
-    write('|                                    |\n'),
-    write('|            0. Main Menu            |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|____________________________________|\n\n'),
-    selectMenuOption(2, ValidOption),
-    sizeAction(ValidOption, GameState, Size).
-
-%Displays the difficulty bot menu
-botDificultyMenu(Difficulty) :-
-    write('\33\[2J'),
-    write('\n\n ____________________________________\n'),
-    write('|                                    |\n'),
-    write('|     _____       _                  |\n'),
-    write('|    |_   _|__ _ | | ____  __ _      |\n'),
-    write('|      | | / _` || ||  _ |/ _` |     |\n'),
-    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
-    write('|                   |_|              |\n'),
-    write('|                                    |\n'),
-    write('|  Choose the computer s difficulty  |\n'),
-    write('|                                    |\n'),
-    write('|          1. Easy (Random)          |\n'),
-    write('|                                    |\n'),
-    write('|         2. Normal (Greedy)         |\n'),
-    write('|                                    |\n'),
-    write('|           0. Main Menu             |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|____________________________________|\n\n'),
-    selectMenuOption(2, Difficulty).
-
-%Displays rules menu
-rulesMenu :-
-    write('\33\[2J'),
-    write('\n\n ____________________________________\n'),
-    write('|                                    |\n'),
-    write('|     _____       _                  |\n'),
-    write('|    |_   _|__ _ | | ____  __ _      |\n'),
-    write('|      | | / _` || ||  _ |/ _` |     |\n'),
-    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
-    write('|                   |_|              |\n'),
-    write('|                                    |\n'),
-    write('|              Rules                 |\n'),
-    write('|                                    |\n'),
-    write('|-Player chooses one of their pieces |\n'),
-    write('|                                    |\n'),
-    write('|-Player chooses the moving position |\n'),
-    write('|                                    |\n'),
-    write('|-Player can only move orthogonally  |\n'),
-    write('|                                    |\n'),
-    write('|                                    |\n'),
-    write('|           0. Main Menu             |\n'),
-    write('|____________________________________|\n\n'),
-    selectMenuOption(0, ValidOption),
-    mainMenu.
 
 %Play
 mainMenuAction(1) :-
@@ -152,6 +54,38 @@ mainMenuAction(1) :-
 %Rules
 mainMenuAction(2) :-
     rulesMenu.
+
+%Exit
+mainMenuAction(0) :-
+    write('\nWe are sad to see you go... :(\n'),
+    write('Thank you for playing!\n').
+
+%Displays rules menu
+rulesMenu :-
+    write('\33\[2J'),
+    write('\n\n ____________________________________________\n'),
+    write('|     _____       _                          |\n'),
+    write('|    |_   _|__ _ | | ____  __ _              |\n'),
+    write('|      | | / _` || ||  _ |/ _` |             |\n'),
+    write('|      |_| |__,_||_|| .__/|__,_|             |\n'),
+    write('|                   |_|                      |\n'),
+    write('|                                            |\n'),
+    write('|              Rules                         |\n'),
+    write('|                                            |\n'),
+    write('|-Choose one of your pieces to move and      |\n'),
+    write('| the moving position                        |\n'),
+    write('|                                            |\n'),
+    write('|-You can only move one position orthogonally|\n'),
+    write('|                                            |\n'),
+    write('|-When no moves available, remove your own   |\n'),
+    write('| piece                                      |\n'),
+    write('|                                            |\n'),
+    write('|-Open a path linking both your player sides |\n'),
+    write('| of the board                               |\n'),
+    write('|           0. Main Menu                     |\n'),
+    write('|____________________________________________|\n\n'),
+    selectMenuOption(0, ValidOption),
+    mainMenu.
 
 %Collaborators
 mainMenuAction(3) :-
@@ -174,29 +108,81 @@ mainMenuAction(3) :-
     write('|            0. Main Menu            |\n'),
     write('|                                    |\n'),
     write('|                                    |\n'),
+    write('|                                    |\n'),
     write('|____________________________________|\n\n'),
     selectMenuOption(0, ValidOption),
     mainMenu.
 
-%Exit
-mainMenuAction(0) :-
-    write('\nWe are sad to see you go... :(\n'),
-    write('If you enjoyed please consider staring our repository https://github.com/anatcruz/FEUP-PLOG-PROJ :D\n').
+%Displays Play menu
+playersMenu :-
+    write('\33\[2J'),
+    write('\n\n ____________________________________\n'),
+    write('|                                    |\n'),
+    write('|     _____       _                  |\n'),
+    write('|    |_   _|__ _ | | ____  __ _      |\n'),
+    write('|      | | / _` || ||  _ |/ _` |     |\n'),
+    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
+    write('|                   |_|              |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|        1. Player VS Player         |\n'),
+    write('|                                    |\n'),
+    write('|        2. Player VS Computer       |\n'),
+    write('|                                    |\n'),
+    write('|       3. Computer VS Computer      |\n'),
+    write('|                                    |\n'),
+    write('|            0. Main Menu            |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|____________________________________|\n\n'),
+    selectMenuOption(3, ValidOption),
+    menuAction(ValidOption).
 
 %Player vs Player
 menuAction(1) :-
-    boardSizeMenu(ValidOption, GameState, Size),
+    boardSizeMenu(GameState, Size),
     printBoard(GameState),
     play(GameState, Size, 1, 'Player', 'Player').
     
 %Player vs Computer
 menuAction(2) :-
-    boardSizeMenu(ValidOption, GameState, Size),
+    boardSizeMenu(GameState, Size), !,
     botDificultyMenu(Difficulty),
-    botAction(Difficulty, GameState, Size).
+    chooseFirstPlayerMenu(First),
+    firstAction(First, GameState, Size, Difficulty).
+
 
 menuAction(3) :-
-    boardSizeMenu(ValidOption, GameState, Size).
+    boardSizeMenu(GameState, Size), !,
+    botsDifficultyMenu(ValidOption),
+    modeAction(ValidOption, GameState, Size).
+
+%Displays board size menu
+boardSizeMenu(GameState, Size):-
+    write('\33\[2J'),
+    write('\n\n ____________________________________\n'),
+    write('|                                    |\n'),
+    write('|     _____       _                  |\n'),
+    write('|    |_   _|__ _ | | ____  __ _      |\n'),
+    write('|      | | / _` || ||  _ |/ _` |     |\n'),
+    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
+    write('|                   |_|              |\n'),
+    write('|                                    |\n'),
+    write('|    Choose the board dimensions     |\n'),
+    write('|                                    |\n'),
+    write('|              1. 6 x 6              |\n'),
+    write('|                                    |\n'),
+    write('|              2. 8 x 8              |\n'),
+    write('|                                    |\n'),
+    write('|            0. Main Menu            |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|____________________________________|\n\n'),
+    selectMenuOption(2, ValidOption),
+    sizeAction(ValidOption, GameState, Size).
 
 sizeAction(1, GameState, Size) :-
     initial(GameState, 6),
@@ -209,16 +195,115 @@ sizeAction(2, GameState, Size) :-
 sizeAction(0, _, _) :-
     fail.
 
-botAction(1, GameState, Size) :-
-    botVSplayerPlay(GameState, Size).
+%Displays the difficulty bot menu
+botDificultyMenu(Difficulty) :-
+    write('\33\[2J'),
+    write('\n\n ____________________________________\n'),
+    write('|                                    |\n'),
+    write('|     _____       _                  |\n'),
+    write('|    |_   _|__ _ | | ____  __ _      |\n'),
+    write('|      | | / _` || ||  _ |/ _` |     |\n'),
+    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
+    write('|                   |_|              |\n'),
+    write('|                                    |\n'),
+    write('|  Choose the computer s difficulty  |\n'),
+    write('|                                    |\n'),
+    write('|          1. Easy (Random)          |\n'),
+    write('|                                    |\n'),
+    write('|         2. Normal (Greedy)         |\n'),
+    write('|                                    |\n'),
+    write('|           0. Main Menu             |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|____________________________________|\n\n'),
+    selectMenuOption(2, ValidOption),
+    difficultyAction(ValidOption, Difficulty).
 
-botAction(2, GameState, Size) :-
-    botVSplayerPlay(GameState, Size).
+difficultyAction(1, Difficulty) :-
+    Difficulty = 'Random'.
 
-botAction(0, _, _) :-
-    mainMenu.
+difficultyAction(2, Difficulty) :-
+    Difficulty = 'Greedy'.
 
-botVSplayerPlay(GameState, Size) :-
+difficultyAction(0, _) :-
+    fail.
+
+chooseFirstPlayerMenu(First) :-
+    write('\33\[2J'),
+    write('\n\n ____________________________________\n'),
+    write('|                                    |\n'),
+    write('|     _____       _                  |\n'),
+    write('|    |_   _|__ _ | | ____  __ _      |\n'),
+    write('|      | | / _` || ||  _ |/ _` |     |\n'),
+    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
+    write('|                   |_|              |\n'),
+    write('|                                    |\n'),
+    write('|Choose the Red Player (first Player)|\n'),
+    write('|                                    |\n'),
+    write('|          1. Player                 |\n'),
+    write('|                                    |\n'),
+    write('|         2. Computer                |\n'),
+    write('|                                    |\n'),
+    write('|           0. Main Menu             |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|                                    |\n'),
+    write('|____________________________________|\n\n'),
+    selectMenuOption(2, First).
+
+firstAction(1, GameState, Size, Difficulty) :-
     printBoard(GameState),
-    play(GameState, Size, 1, 'Player', 'Random').
+    play(GameState, Size, 1, 'Player', Difficulty).
 
+firstAction(2, GameState, Size, Difficulty) :-
+    printBoard(GameState),
+    play(GameState, Size, 1, Difficulty, 'Player').
+
+firstAction(0, _, _, _) :-
+    fail.
+
+botsDifficultyMenu(ValidOption) :-
+    write('\33\[2J'),
+    write('\n\n ____________________________________\n'),
+    write('|                                    |\n'),
+    write('|     _____       _                  |\n'),
+    write('|    |_   _|__ _ | | ____  __ _      |\n'),
+    write('|      | | / _` || ||  _ |/ _` |     |\n'),
+    write('|      |_| |__,_||_|| .__/|__,_|     |\n'),
+    write('|                   |_|              |\n'),
+    write('|                                    |\n'),
+    write('|Choose the difficulty for the computers|\n'),
+    write('|                                    |\n'),
+    write('|          1. Easy VS Easy           |\n'),
+    write('|                                    |\n'),
+    write('|         2. Easy VS Normal          |\n'),
+    write('|                                    |\n'),
+    write('|           3. Normal VS Easy        |\n'),
+    write('|                                    |\n'),
+    write('|            4. Normal VS Normal     |\n'),
+    write('|                                    |\n'),
+    write('|                 0. Main Menu       |\n'),
+    write('|____________________________________|\n\n'),
+    selectMenuOption(3, ValidOption).
+
+modeAction(1, GameState, Size) :-
+    printBoard(GameState),
+    play(GameState, Size, 1, 'Random', 'Random').
+
+modeAction(2, GameState, Size) :-
+    printBoard(GameState),
+    play(GameState, Size, 1, 'Random', 'Greedy').
+
+modeAction(3, GameState, Size) :-
+    printBoard(GameState),
+    play(GameState, Size, 1, 'Greedy', 'Random').
+    
+modeAction(4, GameState, Size) :-
+    printBoard(GameState),
+    play(GameState, Size, 1, 'Greedy', 'Greedy').
+
+modeAction(0, _, _) :-
+    fail.
