f51cbef35af96f859f9b69b6dca83ae8fc70fd98
andrenasx <andre2000.an@gmail.com>
2020-12-06 14:05:07 +0000

    creating all 1x2=2 and 1x2=3 possibilities

--- Crypto-Product/example/cp_example.pl
+++ Crypto-Product/example/cp_example.pl
@@ -10,59 +10,65 @@ writeList([H|T]):-
     write(H), writeList(T).
 
 print(L1, L2, Res):-
-    write('\n'),
     writeList(L1),
     write(' x '),
     writeList(L2),
     write(' = '),
-    writeList(Res).
+    writeList(Res),
+    write('\n').
 
-/* #1 x #2 = ... */
+save_cp1x2_2:-
+    open('cp1x2_2.txt', write, S1),
+    set_output(S1),
+    print_cp1x2_2,
+    flush_output(S1),
+    close(S1).
 
-cp1x2_1:-
-    Vars=[R,G,B],
-    domain(Vars, 0, 9),
-    all_distinct(Vars),
-    R*(G*10+R) #= B*10+G,
-    R#>0, G#>0, B#>0, %first digits of a number can't be 0
-    labeling([], Vars),
-    print([R], [G,R], [B,G]).
+print_cp1x2_2:-
+    cp1x2_2, fail.
+print_cp1x2_2.
 
+save_cp1x2_3:-
+    open('cp1x2_3.txt', write, S1),
+    set_output(S1),
+    print_cp1x2_3,
+    flush_output(S1),
+    close(S1).
+
+print_cp1x2_3:-
+    cp1x2_3, fail.
+print_cp1x2_3.
+
+/* d X dd = dd */
 cp1x2_2:-
     Vars=[R,G,B],
     domain(Vars, 0, 9),
     all_distinct(Vars),
-    G*(G*10+B) #= B*100+R*10+G,
-    R#>0, G#>0, B#>0,
-    labeling([], Vars),
-    print([G], [G,B], [B,R,G]).
+    nth0(_, Vars, N1),
+    nth0(_, Vars, M1),
+    nth0(_, Vars, M2),
+    nth0(_, Vars, R1),
+    nth0(_, Vars, R2),
+    N1 #> 0, M1 #>0, R1 #>0,
+    N1 * (M1*10+M2) #= (R1*10+R2),
+    Nums=[N1,M1,M2,R1,R2],
+    labeling([], Nums),
+    print([N1], [M1,M2], [R1,R2]).
 
+
+/* d X dd = ddd */
 cp1x2_3:-
     Vars=[R,G,B],
     domain(Vars, 0, 9),
     all_distinct(Vars),
-    B*(R*10+G) #= R*100+R*10+G,
-    R#>0, B#>0, 
-    labeling([], Vars),
-    print([B], [R,G], [R,R,G]).
-
-
-/* #2 x #2 = ... */
-
-cp2x2_1:-
-    Vars=[R,G,B],
-    domain(Vars, 0, 9),
-    all_distinct(Vars),
-    (G*10+B)*(G*10+R) #= R*100+B*10+B,
-    R#>0, G#>0, B#>0, 
-    labeling([], Vars),
-    print([G,B], [G,R], [R,B,B]).
-
-cp2x2_2:-
-    Vars=[R,G,B],
-    domain(Vars, 0, 9),
-    all_distinct(Vars),
-    (B*10+G)*(B*10+R) #= (G*100+B*10+R),
-    G#>0, B#>0, 
-    labeling([], Vars),
-    print([B,G], [B,R], [G,B,R]).
+    nth0(_, Vars, N1),
+    nth0(_, Vars, M1),
+    nth0(_, Vars, M2),
+    nth0(_, Vars, R1),
+    nth0(_, Vars, R2),
+    nth0(_, Vars, R3),
+    N1 #> 0, M1 #>0, R1 #>0,
+    N1 * (M1*10+M2) #= (R1*100+R2*10+R3),
+    Nums=[N1,M1,M2,R1,R2,R3],
+    labeling([], Nums),
+    print([N1], [M1,M2], [R1,R2,R3]).
